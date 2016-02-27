25e9b3e69b3809291dfd9def9723398920983d56
Jack Boucher <butticorn@gmail.com>
2016-02-27 03:12:32 -0500

    commit
    way too late whoops

--- untitled3.py
+++ untitled3.py
@@ -9,37 +9,93 @@ import nltk
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.corpus import words
+import os.path
+import csv
 
 filename = 'yelp_academic_dataset_review_small.json'
+ratingcsv = 'rating.csv'
 
-tlist =[]
+if not os.path.isfile(ratingcsv): #create the teachers.csv file, if not already created
+    header = ["Lemma Word", "Lemma Rating"]
+    print("Creating rating.csv to store data")
+    with open(ratingcsv, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+else: 
+    print("Recreating rating.csv...")
+    os.remove(ratingcsv)
+    header = ["Positive Lemma", "Negative Lemma"]
+    print("Creating rating.csv to store data")
+    with open(ratingcsv, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+
+
+
+poslist = []
+poslist2 = []
+neglist = []
+neglist2 = []
+
+stops = set(stopwords.words("English"))
+wordss = set(words.words())
 
 with open(filename) as data_file:    
     data = json.loads(data_file.read())
     
-    for x in range(10): #just working with a small subset
+    for x in range(len(data)): 
     
         reviewText = data[x]['text']
         stars = data[x]['stars']
         
         content = nltk.word_tokenize(reviewText)
         content = [w.lower() for w in content] #normalizing the text
-        
         ps = nltk.PorterStemmer()
-        content = set([ps.stem(w) for w in content if w in words.words() and w not in stopwords.words("english")]) 
+        content = set([ps.stem(w) for w in content if w in wordss and w not in stops]) 
         #filtering out stop words and dashes/ other symbols
         #turn into set to remove duplicate lemmas in the same review
-
-        for word in content: #creates a list of tuples, each tuple contains the lemma and star rating
-            t = (word, stars)
-            tlist.append(t)
+        for word in content:
+            if stars == 1: #creates a list of tuples, each tuple contains the lemma and star rating
+                t = (word, stars)
+                neglist.append(t)
+            if stars == 5:
+                t = (word, stars)
+                poslist.append(t)
             
-    c = Counter(elem[0] for elem in tlist) #counts the occurences of each word
-        #returns a dictionary with the word and count but not the star rating
+               
+    neglist2 = [z for z in neglist if neglist.count(z) > 9] #removes any occurences less than 10
+    poslist2 = [z for z in poslist if poslist.count(z) > 9]
     
-    #r = c.pop(elem[0] for elem in c if c.get(elem[0]) < 10)
-    #tried to remove the ones occuring less than 10 times but it crashes there
+    negcount = Counter(elem[0] for elem in neglist2) #counts the occurences of each word
+    negsort = sorted(negcount.items(), key=lambda item: item[1])
+    if len(negsort) > 500:
+        neglength = len(negsort)
+        fiveneg = negsort[neglength-500]
+    else:
+        fiveneg = negsort
     
-    print(c)              
+    poscount = Counter(elem[0] for elem in poslist2)
+    possort = sorted(poscount.items(), key=lambda item: item[1])
+    if len(possort) > 500:
+        poslength = len(possort)
+        fivepos = possort[poslength-500]
+    else:
+        fivepos = possort
+    for w in fiveneg: 
+        row = [w[0], '1']
+        with open(ratingcsv, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(row)   
+    for w in fivepos: 
+        row = [w[0], '5']
+        with open(ratingcsv, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(row)          
  
-    
+##ls = nltk.LancasterStemmer()
+##stops = set(nltk.corpus.stopwords.words("English"))
+##result = [ls.stem(word.lower()) for word
+##            in nltk.wordpunct_tokenize(text)
+  ##          if word.isalpha()
+    ##        and word not in stops]
+ 
